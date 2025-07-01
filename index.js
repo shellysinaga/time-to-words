11c17dcf7e7979d062d9536628cf03b60084f55d
Shelly Sinaga <shelly.sinaga@gmail.com>
2025-07-01 13:12:29 +0700

    Pair programming exercise submission

--- index.js
+++ index.js
@@ -1,11 +1,98 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
   // TODO: real code goes here!
+  const numberWords = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    21: 'twenty one',
+    22: 'twenty two',
+    23: 'twenty three',
+    24: 'twenty four',
+    25: 'twenty five',
+    26: 'twenty six',
+    27: 'twenty seven',
+    28: 'twenty eight',
+    29: 'twenty nine',
+    30: 'thirty',
+    31: 'thirty one',
+    32: 'thirty two',
+    33: 'thirty three',
+    34: 'thirty four',
+    35: 'thirty five',
+    36: 'thirty six',
+    37: 'thirty seven',
+    38: 'thirty eight',
+    39: 'thirty nine',
+    40: 'forty',
+    41: 'forty one',
+    42: 'forty two',
+    43: 'forty three',
+    44: 'forty four',
+    45: 'forty five',
+    46: 'forty six',
+    47: 'forty seven',
+    48: 'forty eight',
+    49: 'forty nine',
+    50: 'fifty',
+    51: 'fifty one',
+    52: 'fifty two',
+    53: 'fifty three',
+    54: 'fifty four',
+    55: 'fifty five',
+    56: 'fifty six',
+    57: 'fifty seven',
+    58: 'fifty eight',
+    59: 'fifty nine',
+  };
+
+  const splittedTime = time.split(':');
+  const hours = parseInt(splittedTime[0]);
+  const minutes = parseInt(splittedTime[1]);
+  let formattedTime = '';
+
   if (time === '0:00') {
     return 'midnight';
   }
 
-  return 'half past eight';
+  if (time === '12:00') {
+    return 'midday';
+  }
+
+  if (minutes === 0) {
+    return numberWords[hours] + ' o\'clock';
+  }
+
+  if (minutes > 30) {
+    if (minutes === 45) formattedTime = 'quarter to ' + numberWords[hours+1]
+    else {
+      formattedTime = numberWords[60 - minutes] + ' to ' + numberWords[hours+1]
+    }
+  } else if (minutes < 30) {
+    if (minutes === 15) formattedTime = 'quarter past ' + numberWords[hours]
+    else formattedTime = numberWords[minutes] + ' past ' + numberWords[hours]
+  } else {
+    formattedTime = 'half past ' + numberWords[hours];
+  }
+
+  return formattedTime;
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require('./index');
 
 describe('Time to words', () => {
   it('Handles midnight', () => {
@@ -11,8 +11,23 @@ describe('Time to words', () => {
     expect(timeInWords).toBe('half past eight');
   });
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
+  fit('Handles times after 30 mins - 2:45', () => {
+    const timeInWords = convertTimeToWords('2:50');
+    expect(timeInWords).toBe('ten to three');
+  });
+
+  it('Handles midday', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
+  });
+
+  it('Handles times before 30 min - 2:15', () => {
+    const timeInWords = convertTimeToWords('2:15');
+    expect(timeInWords).toBe('quarter past two');
+  });
+
+  it('Handles times with 00 minutes - 2:00', () => {
+    const timeInWords = convertTimeToWords('2:00');
+    expect(timeInWords).toBe('two o\'clock');
   });
 });
